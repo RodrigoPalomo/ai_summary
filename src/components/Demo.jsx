adee46a8a9553aa132f4add3521efd0158e3a9fb
Rodrigo Palomo <repmlm@hotmail.com>
2023-06-22 18:20:21 -0300

    first commit

---
+++ src/components/Demo.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { useState, useEffect } from 'react';
+import { copy, linkIcon, loader, tick } from '../assets';
+
+const Demo = () => {
+    return (
+        <section className='mt-'></section>
+    )
+}
+
+export default Demo
